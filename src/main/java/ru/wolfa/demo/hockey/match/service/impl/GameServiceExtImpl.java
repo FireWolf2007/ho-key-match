c3041d18d036283c188b312232c89e9ca5e81759
Alexander Volf <firewolf_ttyd0@mail.ru>
2018-05-11 22:58:38 +0300

    Change teams in game by user is forbidden

--- src/main/java/ru/wolfa/demo/hockey/match/service/impl/GameServiceExtImpl.java
+++ src/main/java/ru/wolfa/demo/hockey/match/service/impl/GameServiceExtImpl.java
@@ -1,10 +1,14 @@
 package ru.wolfa.demo.hockey.match.service.impl;
 
+import java.util.Optional;
+
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import ru.wolfa.demo.hockey.match.repository.GameRepository;
+import ru.wolfa.demo.hockey.match.security.AuthoritiesConstants;
+import ru.wolfa.demo.hockey.match.security.SecurityUtils;
 import ru.wolfa.demo.hockey.match.service.dto.GameDTO;
 import ru.wolfa.demo.hockey.match.service.mapper.GameMapper;
 import ru.wolfa.demo.hockey.match.web.rest.errors.BadRequestAlertException;
@@ -13,10 +17,6 @@ import ru.wolfa.demo.hockey.match.web.rest.errors.BadRequestAlertException;
 @Transactional
 @Primary
 public class GameServiceExtImpl extends GameServiceImpl {
-
-    public GameServiceExtImpl(GameRepository gameRepository, GameMapper gameMapper) {
-        super(gameRepository, gameMapper);
-    }
 
     /**
      * Save a game.
@@ -29,7 +29,24 @@ public class GameServiceExtImpl extends GameServiceImpl {
         if (gameDTO.getTeam1Id().equals(gameDTO.getTeam2Id())) {
             throw new BadRequestAlertException("team1 and team2 must be different commands","GameDTO", "game.teams.must.differ");
         }
+        if (gameDTO.getId() != null) {
+            Optional<GameDTO> opt = findOne(gameDTO.getId());
+            if (opt.isPresent()) {
+                GameDTO dto = opt.get();
+                if (
+                        (!dto.getTeam1Id().equals(gameDTO.getTeam1Id())
+                        || !dto.getTeam2Id().equals(gameDTO.getTeam2Id()))
+                        && !SecurityUtils.isCurrentUserInRole(AuthoritiesConstants.ADMIN)
+                        ) {
+                    throw new BadRequestAlertException("Change team in game allowed only for admin","GameDTO", "game.teams.change.by.user.forbidden");
+                }
+            }
+        }
         return super.save(gameDTO);
+    }
+
+    public GameServiceExtImpl(GameRepository gameRepository, GameMapper gameMapper) {
+        super(gameRepository, gameMapper);
     }
 
 }
